b9dd53869a4c1cc5f6e4e097c05c998eed534f2e
Guilherme Magalhães <guilherme.maga28@gmail.com>
2026-05-12 20:00:02 -0300

    Update robo.py

--- robo.py
+++ robo.py
@@ -1,22 +1,20 @@
 import datetime
 import pandas as pd
 
-# Simulação de coleta de dados
+# Simula coleta de dados
 def coletar_dados():
     return [
-        {"data": datetime.date.today(), 
-         "evento": "Processamento finalizado", 
-         "status": "OK"},
+        {"data": datetime.date.today(), "evento": "Processamento finalizado", "status": "OK"},
     ]
 
-# Salvar em um CSV
+# Salva os dados em um CSV
 def salvar_relatorio(dados):
     df = pd.DataFrame(dados)
     df.to_csv('dados/relatorio.csv', index=False)
-    print("Relatório salvo com sucesso!")
+    print("✅ Relatório salvo com sucesso!")
 
 # Execução principal
 if __name__ == "__main__":
-    print("Iniciando robô")
+    print("🤖 Iniciando robô...")
     dados = coletar_dados()
     salvar_relatorio(dados)
